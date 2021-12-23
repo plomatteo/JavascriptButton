47962b46a279c46652e418293d3b5c297c1bf7bc
plomatteo <matte.polo@gmail.com>
2021-12-23 14:34:21 +0100

    v 2.1 aggiunta implementazione bottoni da file javascript

---
+++ js/index.js
@@ -0,0 +1,19 @@
+  var pro = ``;
+    pro =` <div class="container">
+    <h1>STEP</h1><br>
+    <div class="counter">
+      <button class="format remove" id="removeStep1">-</button>
+      <input type="number" value="1" class="format" id="displayStep1" }></input>
+      <button class="format add" id="addStep1">+</button>
+    </div>
+  </div>
+  <!-- CONTATORE-->
+  <div class="container">
+    <h1>COUNTER</h1><br>
+    <div class="counter">
+      <button class="format remove" id="removeCounter1">-</button>
+      <input type="number" value="0" class="format" id="displayCounter1" }></input>
+      <button class="format add" id="addCounter1">+</button>
+    </div>
+  </div>`
+    document.getElementById("wrap").innerHTML=pro;
